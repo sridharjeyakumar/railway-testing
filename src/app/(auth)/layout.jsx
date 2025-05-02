1cccaf38b92247fde044caeb89c484385fe41f09
Sridharjeyakumar <sridhardk7@gmail.com>
2025-05-02 12:57:30 +0530

    [login ui chnage]

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -2,7 +2,9 @@ import Footer from "../../components/ui/footer";
 
 export default function AuthLayout({ children }) {
   return (
-    <main className="w-full h-screen bg-[url('/assests/irctc.png')] bg-no-repeat bg-cover bg-right-top scale flex flex-col justify-center items-center">
+    // <main className="w-full h-screen bg-[url('/assests/irctc.png')] bg-no-repeat bg-cover bg-right-top scale flex flex-col justify-center items-center">
+
+    <main>
       {children}
       <div className="ml-10">
         <Footer />
