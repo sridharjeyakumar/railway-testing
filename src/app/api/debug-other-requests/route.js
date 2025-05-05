61311463d93e39296691d4cc9e25c8faa3acd6ed
Sridharjeyakumar <sridhardk7@gmail.com>
2025-05-05 10:00:49 +0530

    cookie fix

--- src/app/api/debug-other-requests/route.js
+++ src/app/api/debug-other-requests/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
-
+export const dynamic = 'force-dynamic'
 export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
